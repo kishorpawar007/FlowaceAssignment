eaee52d774cad3a20b6aab1491f8eff15693aef4
Kishor <Kishor@DESKTOP-V5NCMV5>
2022-10-19 15:48:46 +0530

    Hello. 19-Oct-2022. Flowace.

---
+++ src/test/java/IND_vs_SL/flowace_assignment/AppTest.java
@@ -0,0 +1,20 @@
+package IND_vs_SL.flowace_assignment;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
